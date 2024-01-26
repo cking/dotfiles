751e70fc2ae849d44b215d58141b893749d7632b
Ch. König <me@kurabloodlust.eu>
2024-01-26 13:30:34 +0100

    gentoo support

--- .config/yadm/functions.sh
+++ .config/yadm/functions.sh
@@ -20,6 +20,16 @@ install_package() {
 			done
 			;;
 
+		gentoo)
+			if ! _gentoo_package_installed eselect-repository; then
+				_sudo emerge --ignore-default-opts -n eselect-repository
+			fi
+			while [ $# -gt 0 ]; do
+				_gentoo_install_package "$1"
+				shift
+			done
+			;;
+
 		*)
 			echo "UNSUPPORTED IMPLEMENT NAO!!!"
 			exit 1
@@ -28,6 +38,57 @@ install_package() {
 	esac
 
 }
+
+_gentoo_package_installed() {
+	qlist -I "$1" >/dev/null 2>&1
+	return $?
+}
+
+_gentoo_repo_active() {
+	eselect repository list -i | grep " $1 " >/dev/null 2>&1
+	return $?
+}
+
+_gentoo_install_package() {
+
+	software_name=$1
+	package_name=$( prop gentoo.packages "$software_name.pkg" )
+
+	echo "Installing $software_name..."
+
+	if [ -z "$package_name" ]; then
+		echo "Package unknown, please install it manually now."
+		echo "Press enter to continue..."
+		read -r
+		return
+	fi
+
+	if _gentoo_package_installed "$package_name"; then
+		echo "Already installed!"
+		return
+	fi
+
+	repo=$(prop gentoo.packages "$software_name.repo" )
+
+	if [ -n "$repo" -a "$( _gentoo_repo_active "$repo" ; echo $? )" -gt 0 ]; then
+		echo "Enabling $repo repo...."
+		_sudo eselect repository enable "$repo"
+		_sudo emerge --ignore-default-opts --sync
+		return
+	fi
+
+	unmask=$( prop gentoo.packages "$software_name.unmask" )
+	if _contains "keywords" "$unmask"; then
+		if [ ! -d /etc/portage/package.accept_keywords ]; then
+			mkdir --parents /etc/portage/package.accept_keywords
+		fi
+
+		echo "$package_name ~$( _cpu )" | _sudo tee -a /etc/portage/package.accept_keywords/yadm
+	fi
+
+	_sudo emerge -n --ignore-default-opts $package_name
+}
+
 
 _arch_install_package() {
 
@@ -103,4 +164,23 @@ _sudo() {
 	else
 		su -c "$@"
 	fi
+}
+
+_contains() {
+	keyword=$1
+	shift
+	echo "$*" | grep "$keyword" >/dev/null 2>&1
+	return $?
+}
+
+_cpu() {
+	cpu=$( uname -m )
+	case $cpu in
+		x86_64)
+			echo "amd64"
+			;;
+		*)
+			echo $cpu
+			;;
+	esac
 }
